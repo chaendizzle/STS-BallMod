b340fb8d6fc2c57e70f15c72da013395620ac2d7
Dan Donley <ddonley97@gmail.com>
2025-01-01 19:44:16 -0500

    kickball fix

--- src/main/java/balls/relics/Kickball.java
+++ src/main/java/balls/relics/Kickball.java
@@ -18,9 +18,18 @@ public class Kickball extends AbstractBallRelic {
 
     @Override
     public int onAttackedToChangeDamage(DamageInfo info, int damageAmount) {
-        if (info.type == DamageType.NORMAL && damageAmount < 10) {
+        if (info.type == DamageType.NORMAL) {
             this.flash();
-            addToBot(new DamageAction(info.owner, info, AttackEffect.BLUNT_HEAVY, true));
+
+            // manually clamping since sts runtime not recognizing Math.clamp
+            int damage = info.base;
+            if (damage < 0)
+                damage = 0;
+            if (damage > 8)
+                damage = 8;
+
+            DamageInfo cappedInfo = new DamageInfo(info.owner, damage, info.type);
+            addToBot(new DamageAction(info.owner, cappedInfo, AttackEffect.BLUNT_HEAVY, true));
         }
         return damageAmount;
     }
